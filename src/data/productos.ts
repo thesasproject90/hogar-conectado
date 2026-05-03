0f31408ebedef7af8f66506988f7129bfaf15360
thesasproject90 <marcosdelavega90@gmail.com>
2026-05-03 18:31:30 +0200

    Fix: Enlace de Amazon corregido

--- src/data/productos.ts
+++ src/data/productos.ts
@@ -19,7 +19,7 @@ export const productos: Producto[] = [
     // Esta es la URL de la imagen oficial del servidor de Amazon
     imagenUrl: "https://m.media-amazon.com/images/I/71Y6G67fSXL._AC_SL1500_.jpg",
     // Enlace real directo al producto en Amazon.es
-    linkAmazon: "https://www.amazon.es/dp/B09B8W5FW7",
+    linkAmazon: "https://www.amazon.es/dp/B09B8W5FW7/",
     rating: 4.8,
     opiniones: 25421,
     esTop: true
